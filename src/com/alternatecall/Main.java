dddc6daf9e6cfa9bffb81f7058f0d06ec77b7378
taorusb <taorus@inbox.ru>
2021-02-27 13:39:00 +0300

    updated

--- src/com/alternatecall/Main.java
+++ src/com/alternatecall/Main.java
@@ -1,9 +1,6 @@
 package com.alternatecall;
 
 import com.alternatecall.callingclass.Foo;
-import com.alternatecall.threads.ThreadCallingFirst;
-import com.alternatecall.threads.ThreadCallingSecond;
-import com.alternatecall.threads.ThreadCallingThird;
 
 import java.util.concurrent.*;
 
@@ -11,12 +8,11 @@ public class Main {
     public static void main(String[] args) {
 
         Foo foo = new Foo();
-        Phaser phaser = new Phaser();
         ExecutorService service = Executors.newFixedThreadPool(3);
 
-        service.execute(new ThreadCallingFirst(foo, phaser));
-        service.execute(new ThreadCallingSecond(foo, phaser));
-        service.execute(new ThreadCallingThird(foo, phaser));
+        service.execute(foo::first);
+        service.execute(foo::second);
+        service.execute(foo::third);
 
         service.shutdown();
     }
--- src/com/alternatecall/callingclass/Foo.java
+++ src/com/alternatecall/callingclass/Foo.java
@@ -1,16 +1,33 @@
 package com.alternatecall.callingclass;
 
+import java.util.concurrent.Phaser;
+
 public class Foo {
+
+    private final Phaser phaser;
+
+    public Foo() {
+        phaser = new Phaser(3);
+    }
 
     public void first() {
         System.out.print("first");
+        phaser.arriveAndAwaitAdvance();
+        phaser.arriveAndDeregister();
     }
 
     public void second() {
+        while (phaser.getPhase() < 1) {
+            phaser.arriveAndAwaitAdvance();
+        }
         System.out.print("second");
+        phaser.arriveAndDeregister();
     }
 
     public void third() {
+        while (phaser.getPhase() < 2) {
+            phaser.arriveAndAwaitAdvance();
+        }
         System.out.print("third");
     }
 }
